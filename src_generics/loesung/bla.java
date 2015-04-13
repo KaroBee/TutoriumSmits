3a39c83e561d72626f70dc4b763e2837266161d2
Karo <karol4576@yahoo.de>
2015-04-13 09:43:28 +0200

    ...

---
+++ src_generics/loesung/bla.java
@@ -0,0 +1,10 @@
+package loesung;
+
+public class bla {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src_generics/vorgabe/bla.java
@@ -0,0 +1,10 @@
+package vorgabe;
+
+public class bla {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
